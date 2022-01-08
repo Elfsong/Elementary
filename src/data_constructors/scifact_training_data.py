79d3e8f2b4ada9d7eaaad70d0d906ce6139804d2
Elfsong <dumingzhe@126.com>
2022-01-08 20:21:13 +0800

    update

---
+++ src/data_constructors/scifact_training_data.py
@@ -0,0 +1,97 @@
+# coding: utf-8
+
+import sys
+sys.path.append("..")
+
+from utils import *
+
+scifact_claim_train_dict = get_scifact_dataset("/home/mingzhe/Projects/Elementary/data/Scifact/claims_train.jsonl")
+scifact_claim_dev_dict = get_scifact_dataset("/home/mingzhe/Projects/Elementary/data/Scifact/claims_train.jsonl")
+scifact_claim_test_dict = get_scifact_dataset("/home/mingzhe/Projects/Elementary/data/Scifact/claims_train.jsonl")
+scifact_corpus_dict = get_scifact_corpus("/home/mingzhe/Projects/Elementary/data/Scifact/corpus.jsonl")
+
+def get_lable(claim):
+    evidence = claim["evidence"]
+    if not evidence:
+        return "unknown"
+    else:
+        label = ""
+        for doc_id in evidence:
+            label = evidence[doc_id][0]["label"]
+    
+        if label == "SUPPORT":
+            return "true"
+        else:
+            return "false"
+
+def construct_source_target(claim_instance, doc_candidates):
+    global sources, targets, tpu_data
+    claim_content  = claim_instance["claim"]
+    claim_evidences = claim_instance["evidence"]
+    claim_label = get_lable(claim_instance)
+
+    for doc_id in doc_candidates:
+        source = (f"<s> claim:" + claim_content + " ")
+        target = (claim_label + " ")
+
+        doc_info = scifact_corpus_dict[doc_id]
+        doc_title = doc_info["title"]
+        doc_abstract = doc_info["abstract"]
+
+        selected_index = list()
+        if str(doc_id) in claim_evidences:
+            for evidence_set in claim_evidences[str(doc_id)]:
+                for index in evidence_set["sentences"]:
+                    selected_index += [index]
+        
+        source += "title: " + doc_title + " "
+
+        for index, sentence in enumerate(doc_abstract):
+            sentence = process_evid(sentence)
+            source += f"s{index}: " + sentence + " "
+        target += (" ".join([str(i) for i in selected_index]))
+
+        if not selected_index:
+            target = "unknown "
+
+        if claim_label != "unknown":
+            sources += [source]
+            targets += [target]
+            sources += [source]
+            targets += [target]
+            tpu_data += [f"{source}\t{target}"]
+            tpu_data += [f"{source}\t{target}"]
+        sources += [source]
+        targets += [target]
+        tpu_data += [f"{source}\t{target}"]
+
+# training dataset
+length_list = list()
+sources = []
+targets = []
+tpu_data = []
+
+for claim_id in scifact_claim_train_dict:
+    claim_instance = scifact_claim_train_dict[claim_id]
+
+    # doc candidates
+    doc_candidates = list()
+    gold_doc_candidates = claim_instance["cited_doc_ids"]
+    doc_candidates = gold_doc_candidates
+
+    construct_source_target(claim_instance, doc_candidates)
+
+
+input_path = "/home/mingzhe/Projects/Elementary/input/combine/"
+
+with open(input_path + "train.source", "w") as source_f:
+    for line in sources:
+        source_f.write(line + "\n")
+    
+with open(input_path + "train.target", "w") as target_f:
+    for line in targets:
+        target_f.write(line + "\n")
+
+with open(input_path + "train.data", "w") as data_f:
+    for line in tpu_data:
+        data_f.write(line + "\n")
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,8 @@
+# coding: utf-8
+
+import re
+import json
+import ftfy
 from enum import Enum
 
 class Mode(Enum):
@@ -9,3 +14,32 @@ class Label(Enum):
     TRUE = "true"
     FALSE = "false"
     UNKNOWN = "unknown"
+
+def get_scifact_dataset(file_path):
+    scifact_claim_dict = {}
+    with open(file_path, "r") as f:
+        for line in f.readlines():
+            claim  = json.loads(line)
+            scifact_claim_dict[claim["id"]] = claim
+    return scifact_claim_dict
+
+
+def get_scifact_corpus(file_path):
+    scifact_corpus_dict = {}
+    with open(file_path, "r") as f:
+        for line in f.readlines():
+            doc  = json.loads(line)
+            scifact_corpus_dict[doc["doc_id"]] = doc
+    return scifact_corpus_dict
+
+def process_evid(sentence):
+    sentence = ftfy.fix_text(sentence)
+    sentence = sentence.lower()
+    sentence = re.sub("_", " ", sentence)
+    sentence = re.sub("--", "-", sentence)
+    sentence = re.sub("``", '"', sentence)
+    sentence = re.sub("''", '"', sentence)
+    sentence = re.sub("\t", " ", sentence)
+    sentence = re.sub("\n", " ", sentence)
+    sentence = re.sub("    ", " ", sentence)
+    return sentence
